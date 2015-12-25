595dc60d2deaccd3f6841a506fb007af4679b495
davidrodier <david_rodier@hotmail.com>
2015-12-25 04:26:07 -0500

    update

--- header.js
+++ header.js
@@ -2,7 +2,7 @@ $( document ).ready(function(){$(".button-collapse").sideNav();});
 
 document.write(
 '<nav>'+
-    '<div style="background-color:#4f48ff;" class="nav-wrapper">'+
+    '<div style="background-color:#433ed6;" class="nav-wrapper">'+
       '<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>'+
       '<a id="logo" href="index.html" class="brand-logo center">Introduction</a>' +
       '<ul class="side-nav" id="mobile-demo">'+
